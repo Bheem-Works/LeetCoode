7413a8c2a886348d59101ac3d6710da264ff14ed
bheem-works <shokokomichan098@gmail.com>
2025-05-24 09:29:53 +0545

    Soeking

--- LeetCode/applyTransform/apply.js
+++ LeetCode/applyTransform/apply.js
@@ -1,11 +1,10 @@
-    var map = function (arr,fn) {
+    var map  = function (arr,fn) {
         let result = [];
         for(let i = 0; i < arr.length; i++) {
-            const mana = result.push(fn(arr[i],i));
-            console.log('mana',mana);
+            result.push(fn(arr[i],i));
         }
         return result;
     }
 
-    function plusOne (n)  {return n + 1};
-    console.log(map([1,2,3],plusOne));
+    function onePlus(n) {return n + 1};
+    console.log(map([1,2,3],onePlus));
