0cad5fb83daa741a2ad51da4671e0bcd1b223127
bheem-works <shokokomichan098@gmail.com>
2025-05-30 22:54:06 +0545

    QuestionSolve

--- LeetCode/returnLength/returnLength.js
+++ LeetCode/returnLength/returnLength.js
@@ -5,9 +5,8 @@
  * @return {number}
  */
 var argumentsLength = function(...args) {
-    
+        return args.length;
 };
 
-/**
- * argumentsLength(1, 2, 3); // 3
- */
+
+ console.log(argumentsLength(1, 2, 3)); // 3
