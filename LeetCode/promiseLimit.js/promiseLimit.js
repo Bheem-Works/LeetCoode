f4aaea58520f304fb8983290c9b0c02a62594f91
bheem-works <shokokomichan098@gmail.com>
2025-06-01 16:59:47 +0545

    PostCase

--- LeetCode/promiseLimit.js/promiseLimit.js
+++ LeetCode/promiseLimit.js/promiseLimit.js
@@ -1,10 +1,28 @@
-    var timeLimit = function(fn, t) {
-    return async function(...args) {
-        return Promise.race([
-            fn(...args),
-            new Promise((_, reject) =>
-                setTimeout(() => reject("Time Limit Exceeded"), t)
-            )
-        ]);
-    };
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let boolean = false;
+    
+    return function(...args){
+        if(!boolean) {
+            boolean = true;
+            return fn(...args);
+    } else {
+        return undefined;
+    }
+    }
 };
+
+
+ let fn = (a,b,c) => (a + b + c)
+ let onceFn = once(fn)
+ 
+  console.log(onceFn(1, 2, 3)); // 6
+
+// ❌ Next calls
+console.log(onceFn(1, 2, 8)); // false
+console.log(onceFn(5, 5, 5)); // false
+
+
